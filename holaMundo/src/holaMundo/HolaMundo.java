a69a0872e0cf26d7efb4274f0b975ff1cf65b5c3
rasendin <raulsendin13@gmail.com>
2023-01-17 12:15:54 +0100

    Update HolaMundo.java

--- holaMundo/src/holaMundo/HolaMundo.java
+++ holaMundo/src/holaMundo/HolaMundo.java
@@ -2,9 +2,19 @@ package holaMundo;
 
 public class HolaMundo {
 
-	public static void main(String[] args) {
+	public static void ejemplosComentarios() {
+		/**
+		 * Para poner el comentario en azul
+		 */
+		/*
+		 * Este es mi primer programa
+		 */
 		System.out.println("Hola mundo");
+		// System.out.println("Comentario individual");
+	}
 
+	public static void main(String[] args) {
+		 ejemplosComentarios();
 	}
 
 }
