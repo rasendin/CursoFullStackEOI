b8326ff355f5b61674cf08c0d5b1a5f354c50225
rasendin <raulsendin13@gmail.com>
2023-01-17 22:03:45 +0100

    Ejercicios

---
+++ ejercicios/src/ejercicios/ejerciciosFullStack.java
@@ -0,0 +1,35 @@
+package ejercicios;
+
+public class ejerciciosFullStack {
+
+	public static void ejercicio1() {
+
+		int num1 = 36;
+		int num2 = 27;
+		int resultado = num1 + num2;
+
+		System.out.println("El resultado de sumar " + num1 + " + " + num2 + " da como resultado " + resultado);
+
+	}
+
+	public static void ejercicio2() {
+
+		int num1 = 15;
+		int num2 = 4;
+
+		System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+		System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
+		System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));
+		System.out.println(num1 + " * " + num2 + " = " + (num1 * num2));
+		System.out.println(num1 + " % " + num2 + " = " + (num1 % num2));
+
+	}
+
+	public static void main(String[] args) {
+
+		// ejercicio1();
+		ejercicio2();
+
+	}
+
+}
