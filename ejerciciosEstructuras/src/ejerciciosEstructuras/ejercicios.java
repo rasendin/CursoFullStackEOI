4111a26d5e1c811b8fbe494dc441a452cb594b9e
rasendin <raulsendin13@gmail.com>
2023-01-23 12:19:11 +0100

    Update ejercicios.java

--- ejerciciosEstructuras/src/ejerciciosEstructuras/ejercicios.java
+++ ejerciciosEstructuras/src/ejerciciosEstructuras/ejercicios.java
@@ -198,6 +198,47 @@ public class ejercicios {
 		sc.close();
 	}
 	
+	public static void ejercicio12Grupo() {
+		final double MENOR_EDAD = 0.25;
+		final double DESCUENTO_SOCIO = 0.40;
+		final double DESCUENTO_JUBILADO = 0.75;
+		final int PRECIO_ENTRADA = 50;
+		final int MAYORIA_EDAD = 18;
+		final int EDAD_JUBILACION = 65;
+		Scanner sc = new Scanner (System.in);  // Para leer datos por pantalla
+		double precioEntrada = PRECIO_ENTRADA;  // Precio estándar sin descuentos
+		double precioTotalGrupo = 0;  // Almacenará el precio total
+		System.out.println("¿Cuántos sois?");
+		int personas = sc.nextInt();  // Ya tengo las personas
+		sc.nextLine();
+		int personasAtendidas = 0;
+		while(personasAtendidas<personas) {
+			System.out.println("Datos de la persona " + (personasAtendidas+1));
+			System.out.println("Introduzca su edad:");
+			int edad = sc.nextInt();
+			sc.nextLine();
+			precioEntrada = PRECIO_ENTRADA;  // Volver a poner el precio que toca para ver si se aplican descuentos
+			if(edad<MAYORIA_EDAD) {
+				precioEntrada = precioEntrada * (1-MENOR_EDAD);  
+			} else if(edad>=MAYORIA_EDAD && edad<EDAD_JUBILACION) {
+				System.out.println("Es usted socio (sí/no):");
+				String respuesta = sc.nextLine();
+				if(respuesta.equalsIgnoreCase("si") || respuesta.equalsIgnoreCase("sí") || respuesta.equalsIgnoreCase("s")) {
+					precioEntrada = precioEntrada * (1 - DESCUENTO_SOCIO);
+				}
+			} else {
+				precioEntrada = precioEntrada * (1-DESCUENTO_JUBILADO);
+			}
+			precioTotalGrupo += precioEntrada;
+			System.out.println("Precio de la entrada de la persona " + (personasAtendidas+1) + " es " + precioEntrada);
+			System.out.println("Precio del grupo hasta el momento: " + precioTotalGrupo);
+			personasAtendidas++;
+		}
+				
+		System.out.println("El precio FINAL del grupo es: " + precioTotalGrupo);
+		sc.close();
+	}
+	
 	public static void ejercicio13() {
 		int numero = 20;
 		while(numero>0) {
@@ -248,7 +289,8 @@ public class ejercicios {
 		// ejercicio13();
 		// ejercicio14();
 		// ejercicio13b();
-		ejercicio12();
+		// ejercicio12();
+		ejercicio12Grupo();
 	}
 
 }
