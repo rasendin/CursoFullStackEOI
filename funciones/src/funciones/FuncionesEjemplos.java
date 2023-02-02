b1db1d1a9701a010113a539f103fa9995ed93e13
rasendin <raulsendin13@gmail.com>
2023-02-02 20:08:40 +0100

    Funciones

--- funciones/src/funciones/FuncionesEjemplos.java
+++ funciones/src/funciones/FuncionesEjemplos.java
@@ -1,5 +1,13 @@
 package funciones;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Scanner;
+
 public class FuncionesEjemplos {
 	
 	public static void imprimirHola() {
@@ -57,6 +65,136 @@ public class FuncionesEjemplos {
 		return resultado;
 	}
 	
+	public static void parametrosValor(int numero, int numero2) {
+		numero++;
+		numero2++;
+	}
+
+	public static void parametrosReferencia(int[] valores) {
+		valores[0]++;
+	}
+	
+	public static void ejemplosMath() {
+		double n1 = -23.40;
+		double n2 = 2.449532;
+
+		System.out.println(Math.abs(n1)); // 23.4 El número siempre en positivo
+		System.out.println(Math.round(n2)); // 2
+		System.out.println(Math.round(n1)); // -23
+		System.out.println(Math.ceil(n2)); // 3 Redondea hacia arriba siempre
+		System.out.println(Math.ceil(n1)); // -23
+		System.out.println(Math.floor(n2)); // 2 Redondea hacia abajo siempre
+		System.out.println(Math.floor(n1)); // -24
+		System.out.println(Math.max(23, 43)); // 43
+		System.out.println(Math.min(4, 5)); // 4
+		System.out.println(Math.sqrt(25)); // 5
+		System.out.println(Math.pow(3, 2)); // 9
+		System.out.println(Math.pow(3, 3)); // 27
+
+	}
+	
+	public static void ejemplosString() {
+		String s1 = "abc";
+		String s2 = "def";
+		String s3 = "ghi";
+		String concatenada = s1.concat(s2).concat(s3);
+		String concatenada2 = s1 + s2 + s3;
+		System.out.println(concatenada); // abcdefghi
+		System.out.println(concatenada2); // abcdefghi
+		s1.concat(s2).concat(s3); // s1 => abcdefghi? NO
+		System.out.println(s1); // abc
+		s1 = s1.concat(s2).concat(s3); // s1 => abcdefghi? SI
+		System.out.println(s1); // abcdefghi
+
+		String[] nombres = { "Paco", "Pepa", "Dani" };
+		String nombresJuntos = String.join(", ", nombres);
+		System.out.println(String.join(", ", nombres));
+		System.out.println(nombresJuntos);
+
+		String cad1 = "alicante";
+		String cad2 = "dani";
+		String cad3 = "Alava";
+		System.out.println(cad1.compareTo(cad2)); // -3
+		System.out.println(cad1.compareTo(cad3)); // 32
+		System.out.println(cad1.compareToIgnoreCase(cad3)); // 8
+
+		String s = "Mi perro se llama Comeniños";
+		System.out.println(s.indexOf("perro")); // 3
+		System.out.println(s.indexOf("gato")); // -1 Valores no encontrados
+		System.out.println(s.indexOf("perro", 4)); // -1 no la encuentra más
+
+		String s4 = "Mi perro se llama Comeniños";
+		System.out.println(s4.contains("perro")); // true
+		System.out.println(s4.contains("gato")); // false
+
+		String s5 = "Mi perro se llama Comeniños y mi perrose también";
+		String s6 = s5.replace("perro", "koalaaaaaa");
+		System.out.println(s6); // Mi koalaaaaaa se llama Comeniños y mi koalaaaaaase también
+
+		String s7 = "HOLAAAAA";
+		System.out.println(s7.toLowerCase()); // holaaaaa
+		System.out.println(s7.toUpperCase()); // HOLAAAAA
+
+		String cuenta = "111;Fran;27/07/1976;ES;10000";
+		String[] partes = cuenta.split(";"); // partes[0] = "111", partes[1]="Fran"...
+		System.out.println(partes[1] + " tiene " + partes[4] + " euros");
+	}
+	
+	public static void buscarArrayInteger() {
+		Integer[] numeros = { 5, 9, 14, 7, 2 };
+		boolean encontrado = Arrays.stream(numeros).anyMatch(e -> e == 5); // true
+		boolean encontrado2 = Arrays.stream(numeros).anyMatch(e -> e == 8); // false
+		System.out.println(encontrado);
+		System.out.println(encontrado2);
+		System.out.println(Arrays.asList(numeros).indexOf(14)); // posición
+	}
+
+	public static void ejemplosFechas() {
+		Date noUsar = new Date(); // No usar nunca Date si trabajamos en un proyecto Java 8+
+		LocalDate fecha = LocalDate.now();
+		System.out.println(fecha); // 2023-02-02
+		LocalDate hoyMas15 = fecha.plusDays(15);
+		System.out.println(hoyMas15);
+		LocalDate anteAyer = fecha.minusDays(2);
+		System.out.println(anteAyer);
+		System.out.println(fecha.getDayOfMonth() + " " + fecha.getMonthValue() + " " + fecha.getYear());
+		System.out.println(fecha.getDayOfYear()); // día del año 1-365,366
+		LocalDate fechaConcierto = LocalDate.of(2024, 3, 30); // 30-3-2024
+		if (fecha.isBefore(fechaConcierto) || fecha.isEqual(fechaConcierto)) {
+			System.out.println("Puedes comprar entradas");
+		} else {
+			System.out.println("El concierto ya pasó");
+		}
+
+		String[] dias = { "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo" };
+		LocalDate fechaHoy = LocalDate.now();
+		String diaHoy = dias[fechaHoy.getDayOfWeek().getValue()-1];
+		System.out.printf("Hoy es %s, %02d/%02d/%d\n", diaHoy, fechaHoy.getDayOfMonth(), fechaHoy.getMonthValue(),
+				fechaHoy.getYear()); // Hoy es Jueves, 02/02/2023
+		
+		// Usando un patrón del sistema
+		System.out.println(fecha.format(DateTimeFormatter.ofPattern("'Hoy es' MMMM eeee, dd/MM/y")));
+		
+		LocalTime ahora = LocalTime.now();
+		System.out.println(ahora);
+		LocalTime dentro1Hora = ahora.plusHours(1);
+		System.out.println(dentro1Hora);
+
+		LocalDateTime fechaAhora = LocalDateTime.now();
+		System.out.println(fechaAhora);
+
+	}
+	
+	public static void cogerFechaPorConsola() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduzca su fecha de nacimiento (dd/mm/yyyy):");
+		String fechaCadena = sc.nextLine().replace("-", "/");  // "27-07-76", "27/07/76", "27/07/1976"
+		LocalDate fecha = LocalDate.parse(fechaCadena, DateTimeFormatter.ofPattern("d/M/y"));
+		System.out.println(fecha);
+		sc.close();
+	}
+
+	
 	public static void main(String[] args) {
 		
 		if(args.length>0)
@@ -85,6 +223,21 @@ public class FuncionesEjemplos {
 		} else {
 			System.out.println("La suma no es mayor que 10");
 		}
+		
+		int var1 = 5;
+		int var2 = 10;
+		parametrosValor(var1, var2);
+		System.out.println("Var1: " + var1 + " Var2: " + var2); // 6 y 11 - 5 y 10? => 5 y 10
+
+		int[] var1ref = { var1 }; // {5}
+		parametrosReferencia(var1ref); // {6}
+		System.out.println(var1ref[0]); // 6
+
+		ejemplosMath();
+		ejemplosString();
+		// buscarArrayInteger();
+		ejemplosFechas();
+		cogerFechaPorConsola();
 		
 	}
 	
--- funciones/src/funciones/FuncionesEjercicios.java
+++ funciones/src/funciones/FuncionesEjercicios.java
@@ -1,29 +1,84 @@
 package funciones;
 
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class FuncionesEjercicios {
 
-	// Ejercicio 6. Crea una función que reciba un array de números y devuelva su media
+	//6. Crea una función que reciba un array de números y devuelva su media
 	public static double media(double... numeros) {
 		double resultado = 0;
 		for(double numero : numeros) {
 			resultado += numero;
 		}
-		return resultado/numeros.length;
+		return resultado/numeros.length;		
 	}
 	
 	public static double ejercicio6(double...numeros) {
-		return Arrays.stream(numeros).average().getAsDouble();	// .average te saca la media . getAsDouble me lo devuelve en Double
+		return Arrays.stream(numeros).average().getAsDouble();
+	}
+	
+	/**
+	 * Te devuelve un elemento aleatoriamente dentro de un conjunto de elementos
+	 * En este caso trabajamos con cadenas
+	 * @param cadenas
+	 * @return
+	 */
+	public static String devolverAzar(String[] cadenas) {
+		return cadenas[new Random().nextInt(cadenas.length)];
+	}
+	
+	/**
+	 * Mostrar por pantalla todos los elementos dentro de un conjunto
+	 * @param cadenas
+	 */
+	public static void mostrarElementos(String[] cadenas) {
+		/*for(String cadena : cadenas) {  // Programación estructurada
+			System.out.println(cadena);
+		}*/
+		Arrays.stream(cadenas).forEach(e->System.out.println(e));  // Programación funcional
+	}
+	
+	/**
+	 * Crea una función que reciba un array de cadenas por parámetro y devuelva una de las
+	 * cadenas al azar. Pista: puedes generar un número aleatorio entre 0 y la última
+	 * posición del array usando new Random().nextInt(array.length).
+	 * A continuación pide al usuario que adivine la palabra seleccionada (tiene 3 intentos,
+	 * usa un bucle). Si la acierta felicítale, y si no, dile cual era la correcta.
+	 */
+	public static void ejercicio7() {
+		Scanner sc = new Scanner(System.in);
+		final int MAXIMO_INTENTOS = 3;
+		boolean acierto = false;
+		String[] palabras = {"hola", "adiós", "mundo", "cruel", "fran", "eoi", "java", "javascript","html", "css"};
+		String palabraAleatoria = devolverAzar(palabras);
+		int intentos = 0;
+		do {
+			System.out.println("Introduzca una palabra (le quedan " + (MAXIMO_INTENTOS-intentos) + " intento" + ((MAXIMO_INTENTOS-intentos)==1?"): ":"s): "));
+			mostrarElementos(palabras);
+			String palabra = sc.nextLine();
+			if(palabra.equals(palabraAleatoria)) {
+				acierto = true;
+			}
+			intentos++;
+		}while(intentos<MAXIMO_INTENTOS && !acierto);
+		if(acierto) {  // He salido del bucle acertando
+			System.out.println("Enhorabuena!. Acertaste.");
+		} else {
+			System.out.println("Gastaste tus intentos. La palabra era " + palabraAleatoria);
+		}
+		sc.close();
 	}
 	
 	/**
 	 * Crea una función que reciba un número indeterminado de parámetros enteros y
-	 * devuelva el máximo. Ten en cuenta que si recibes cero números, debes
-	 * devolver cero también
-	 * @param args
+	 * devuelva el máximo. Ten en cuenta que si recibes cero números, debes devolver cero
+	 * también.
 	 */
-	
 	public static int ejercicio8(int...numeros) {
 		int maximo = Integer.MIN_VALUE;
 		if(numeros.length==0) {
@@ -37,8 +92,61 @@ public class FuncionesEjercicios {
 		return maximo;
 	}
 	
-	public static int ejercicio8Funcional(int...numeros) {
+	public static int ejercicio8funcional(int...numeros) {
 		return Arrays.stream(numeros).max().orElse(0);
+	}
+	
+	/**
+	 * Crea una función que reciba una cadena y una subcadena por parámetro y devuelva
+	 * cuantas veces aparece la subcadena dentro de la cadena. Para ello debes saber que
+	 * como segundo parámetro a indexOf le puedes pasar la posición a partir de la cual
+	 * buscar (empezaríamos en cero). Cuando te devuelva -1 es que ya no la ha encontrado
+	 * (utiliza un bucle).
+	 * Por ejemplo, si recibimos “cocinando cocos con chocolate” y “co” devolveremos el
+	 * número 5.
+	 * @param cadena
+	 * @param subcadena
+	 * @return
+	 */
+	public static int ejercicio9(String cadena, String subcadena) {
+		int posicionBuscar = 0;  // variable que almacena a partir de que posición buscar la subcadena
+		int resultado = 0;  // veces que encuentra la subcadena
+		while((posicionBuscar=cadena.indexOf(subcadena, posicionBuscar))!=-1) {  // Lo encuentra
+			resultado++;
+			posicionBuscar++;
+		}
+		return resultado;
+	}
+	
+	public static int ejercicio9b(String cadena, String subcadena) {
+		int numero=-1;
+		int veces = 0;
+		numero=cadena.indexOf(subcadena);		
+		while(numero!=-1) {
+			veces++;
+			numero=cadena.indexOf(subcadena,numero+1);
+		}
+		return veces;
+	}
+	
+	public static int ejercicio9c(String cadena, String subcadena) {
+		// Busca a partir de que acabe la cadena que estoy buscando
+		int veces = 0;
+		Pattern patron = Pattern.compile(subcadena);
+		Matcher coincidencia = patron.matcher(cadena);
+		while(coincidencia.find()) {
+			veces++;
+		}
+		return veces;
+	}
+	
+	/**
+	 * Crea una función que reciba un array de cadenas y te lo devuelva ordenado
+	 * alfabéticamente de la ‘z’ a la ‘a’
+	 */
+	public static void ejercicio12(String[] cadenas) {
+		//Arrays.sort(cadenas);  // Orden ascendente y se modifica fuera del método
+		Arrays.sort(cadenas, Comparator.reverseOrder());  // orden descendente
 	}
 	
 	public static void main(String[] args) {
@@ -52,15 +160,32 @@ public class FuncionesEjercicios {
 		double otraMedia = media(numeros);
 		System.out.println(otraMedia);*/
 		
-		// Ejercicio8
-		int maximo = ejercicio8(2,8,99,4,1000,9,8);
-		System.out.println(maximo);	// 1000
-		System.out.println(ejercicio8());	// 0
-		System.out.println(ejercicio8(-2,-3,-10));	// -2
-		System.out.println(ejercicio8Funcional(2,8,99,4,1000,9,8));	// 1000
-		System.out.println(ejercicio8());	// 0
-		System.out.println(ejercicio8(-2,-3,-10));	// -2
+		//ejercicio7();
 		
+		// Ejercicio 8
+		/*int maximo = ejercicio8(2,8,99,4,1000,9,8);
+		System.out.println(maximo);  // 1000
+		System.out.println(ejercicio8());  // 0
+		System.out.println(ejercicio8(-2,-3,-10));  // -2
+		System.out.println(ejercicio8funcional(2,8,99,4,1000,9,8));  // 1000
+		System.out.println(ejercicio8funcional());  // 0
+		System.out.println(ejercicio8funcional(-2,-3,-10));  // -2
+		*/
+		
+		int veces = ejercicio9("cocinando cocos con chocolate","co");  // debe devolver 5
+		System.out.println(veces);  // debe imprimir 5
+		System.out.println(ejercicio9b("cocinando cocos con chocolate","co"));  // 5
+		System.out.println(ejercicio9c("cocinando cocos con chocolate","co"));  // 5
+		System.out.println(ejercicio9("cocinando cococos con chococolate","coco"));  // 3
+		System.out.println(ejercicio9b("cocinando cococos con chococolate","coco"));  // 3
+		System.out.println(ejercicio9c("cocinando cococos con chococolate","coco"));  // 2
+		
+		String[] meses = {"Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"};
+		ejercicio12(meses);  // meses se modifica el orden dentro del método
+		for(String mes: meses) {
+			System.out.println(mes);
+		}
+		System.out.println(Arrays.toString(meses));
 	}
 
 }
