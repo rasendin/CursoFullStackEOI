1c617f2450bc88e81a4bd3a36a2d8f10a43510f7
rasendin <raulsendin13@gmail.com>
2023-02-15 14:23:49 +0100

    xml

---
+++ xml/src/main/java/com/raul/xml/App.java
@@ -0,0 +1,52 @@
+package com.raul.xml;
+
+import java.io.File;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class App 
+{
+	
+	public static void leerXmlFichero(String nombreFichero) {
+		try {
+			File inputFile = new File(nombreFichero);
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(inputFile);  // Comprueba que es un XML valido
+			doc.getDocumentElement().normalize();
+			System.out.println("Elemento base : " + doc.getDocumentElement().getNodeName());
+			NodeList nList = doc.getElementsByTagName("asignatura");
+			System.out.println();
+			System.out.println("Recorriendo asignaturas...");
+			for (int temp = 0; temp < nList.getLength(); temp++) {
+				Node nNode = nList.item(temp);
+				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+					Element eElement = (Element) nNode;
+					System.out.println("Codigo: " + eElement.getAttribute("id"));
+					System.out.println("Nombre: " + eElement.getElementsByTagName("nombre").item(0).getTextContent());
+					System.out.println(
+							"Ciclo: " + eElement.getElementsByTagName("cicloFormativo").item(0).getTextContent());
+					System.out.println("Curso: " + eElement.getElementsByTagName("curso").item(0).getTextContent());
+					System.out
+							.println("Profesor: " + eElement.getElementsByTagName("profesor").item(0).getTextContent());
+					System.out.println();
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+    public static void main( String[] args ) {
+    	
+    	leerXmlFichero("asignaturas.xml");
+    	
+    }
+}
